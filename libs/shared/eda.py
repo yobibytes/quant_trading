88874cc5b45de4fa0cba679afe2daa7f580431fe
yobibytes <miaomiaokatze-1@yahoo.com>
2022-02-16 16:01:40 +0100

    Create eda.py

---
+++ libs/shared/eda.py
@@ -0,0 +1,40 @@
+# Function to show summary stats and distribution for a column
+def show_distribution(var_data):
+    from matplotlib import pyplot as plt
+
+    # Get statistics
+    min_val = var_data.min()
+    max_val = var_data.max()
+    mean_val = var_data.mean()
+    med_val = var_data.median()
+    mod_val = var_data.mode()[0]
+
+    print(var_data.name,'\nMinimum:{:.2f}\nMean:{:.2f}\nMedian:{:.2f}\nMode:{:.2f}\nMaximum:{:.2f}\n'.format(min_val,
+                                                                                            mean_val,
+                                                                                            med_val,
+                                                                                            mod_val,
+                                                                                            max_val))
+
+    # Create a figure for 2 subplots (2 rows, 1 column)
+    fig, ax = plt.subplots(2, 1, figsize = (10,4))
+
+    # Plot the histogram   
+    ax[0].hist(var_data)
+    ax[0].set_ylabel('Frequency')
+
+    # Add lines for the mean, median, and mode
+    ax[0].axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+
+    # Plot the boxplot   
+    ax[1].boxplot(var_data, vert=False)
+    ax[1].set_xlabel('Value')
+
+    # Add a title to the Figure
+    fig.suptitle(var_data.name)
+
+    # Show the figure
+    fig.show()
